e9ec274965d94c44cef51c28aed7243bc6597095
Mahesh Perla <maheshp212@gmail.com>
2018-03-29 09:47:07 +0530

    initial commit

---
+++ batch15/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router'
+
+import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { VariablesComponent } from './variables/variables.component';
+
+
+const routes:Routes = [
+	{path:'intro', component:IntroComponent},
+	{path:'variables', component:VariablesComponent},
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    IntroComponent,
+    VariablesComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
