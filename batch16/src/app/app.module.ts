90403789fd8f803b68d7f448cadeeeacc66b0545
Mahesh Perla <maheshp212@gmail.com>
2018-04-20 08:31:54 +0530

    variables and routes

--- batch16/src/app/app.module.ts
+++ batch16/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router'
+
+
 
 
 import { AppComponent } from './app.component';
 import { IntroComponent } from './intro/intro.component';
 import { VariablesComponent } from './variables/variables.component';
 
-
+const routes:Routes = [
+    {path:'intro', component:IntroComponent},
+    {path:'variables', component:VariablesComponent},
+  ];
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +20,8 @@ import { VariablesComponent } from './variables/variables.component';
     VariablesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- batch16/src/app/variables/variables.component.ts
+++ batch16/src/app/variables/variables.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-variables',
   templateUrl: './variables.component.html',
   styleUrls: ['./variables.component.css']
 })
-export class VariablesComponent implements OnInit {
+export class VariablesComponent {
+	fname:string = 'qshore';
+	lname:string = 'tech';
+	age:number = 34;
+	fruits:string[] = ['tech' , 'kiwi', 'orange'];
+	user:any = {field:'tech', show:'hellow'};
+	intrst:null = null;
+	gender:boolean = true;
+	undf;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	a:number = 2;
+	b:number = 3;
 }
